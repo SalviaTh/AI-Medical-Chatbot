241c7833a31f95833a5674cbf35fc4554c1d5650
Thingbaijam Salvia <salviathingbaijam@gmail.com>
2025-11-07 11:06:03 +0530

    code added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI PROJECT',
+    version='0.0.0',
+    author='Thingbaijam Salvia',
+    author_email='salviathingbaijam@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
